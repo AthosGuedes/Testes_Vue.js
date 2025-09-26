7b366d3a89ab1a3159c0874918375d954f2f4407
AthosGuedes <athosgneto@gmail.com>
2025-09-26 00:09:20 -0300

    primeiras páginas

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Página Inicial 🏠</h1>
+    <p>Bem-vindo ao meu projeto com Nuxt 3!</p>
+
+    <NuxtLink to="/sobre">
+      Ir para Sobre
+    </NuxtLink>
+  </div>
+</template>
---
+++ app/pages/sobre.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Página Sobre 📄</h1>
+    <p>Aqui é a página de sobre.</p>
+
+    <NuxtLink to="/">
+      Voltar para Home
+    </NuxtLink>
+  </div>
+</template>
