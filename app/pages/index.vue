2a7f9a7f4a448b3db028e895ee028120996a07db
AthosGuedes <athosgneto@gmail.com>
2025-09-27 21:40:22 -0300

    add tailwindCSS

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
     <h1>Página Inicial 🏠</h1>
     <p>Bem-vindo ao meu projeto com Nuxt 3!</p>
 
-    <NuxtLink to="/sobre">
+    <NuxtLink to="/sobre" className='underline text-blue-500'>
       Ir para Sobre
     </NuxtLink>
   </div>
